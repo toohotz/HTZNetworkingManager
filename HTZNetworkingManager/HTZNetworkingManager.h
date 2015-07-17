fd4cb179b5ee4118a6bb3156de49b6d5e1721212
Kemar White <toohotz@me.com>
2015-07-17 13:55:19 -0400

    Initial Commit

---
+++ HTZNetworkingManager/HTZNetworkingManager.h
@@ -0,0 +1,19 @@
+//
+//  HTZNetworkingManager.h
+//  HTZNetworkingManager
+//
+//  Created by Kemar White on 7/17/15.
+//  Copyright (c) 2015 toohotz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTZNetworkingManager.
+FOUNDATION_EXPORT double HTZNetworkingManagerVersionNumber;
+
+//! Project version string for HTZNetworkingManager.
+FOUNDATION_EXPORT const unsigned char HTZNetworkingManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTZNetworkingManager/PublicHeader.h>
+
+
