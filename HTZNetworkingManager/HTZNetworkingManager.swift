1f8e71e75f635b4b02139d4087030b34cf365d7a
Kemar White <gailsx@gmail.com>
2016-02-18 21:11:08 -0500

    Refactored sendData method
    Made method more robust in terms of expected response.

--- HTZNetworkingManager/HTZNetworkingManager.swift
+++ HTZNetworkingManager/HTZNetworkingManager.swift
@@ -23,6 +23,12 @@ public enum HTTPRequestType: Int {
     }
 }
 
+public enum ResponseType {
+    case JSON
+    case String
+    case RawData
+}
+
 public class HTZNetworkingFacade {
 
     /// The networking facade shared instance.
@@ -76,38 +82,49 @@ public class HTZNetworkingManager: Manager {
     }
 
     /**
-     Sends data to a specific endpoint using an **HTTPRequestType** request.
+     Sends data to a specified endpoint (if provided) using an **HTTPRequestType** request.
 
      - parameter endpoint:       The endpoint for the URL HTTP request.
      - parameter dataParameters: Optional dictionary information to send along with the request.
-     - parameter httpMethod:     **HTTPMethod** to define the request type.
-     - parameter JSONData:       Closure containing JSON data or an error if the request could no be completed.
+     - parameter responseType:   The desired response type (ie. JSON for a JSON response type, String for an expected string response type)
+     - parameter httpMethod:      **HTTPMethod** to define the request type.
+     - parameter resultData:     Closure containing result data or an error if the request could no be completed.
      */
-    public func sendDataWithEndPoint(endpoint: String?, dataParameters: [String : AnyObject]?, httpMethod: HTTPRequestType, JSONData: (Result<AnyObject, NetworkingError>) -> () )
+    public func sendDataWithEndPoint(endpoint: String?, dataParameters: [String : AnyObject]?, responseType: ResponseType, httpMethod: HTTPRequestType, resultData: (Result<AnyObject, NetworkingError>) -> () )
     {
         guard let setURL = self.baseURL else {
             print("A base URL has not been set, cancelling request.")
             return
         }
-        if endpoint == nil || endpoint?.isEmpty == true {
-            Alamofire.request(Method(rawValue: HTTPRequestType(rawValue: httpMethod.rawValue)!.name)!, setURL, parameters: dataParameters, encoding: .URL, headers: nil).responseJSON(completionHandler: { (jsonResponse) -> Void in
-                if let finalResponse = jsonResponse.result.value {
-                    JSONData(.Success(finalResponse))
-                } else {
-                    if let errorOccurred = jsonResponse.result.error {
-                        JSONData(.Failure(NetworkingError.ResponseError(errorOccurred.localizedDescription)))
-                    }
+        let requestURL = (endpoint != nil) ? "\(setURL)\(endpoint!)" : setURL
+        // Response type checking
+        switch responseType {
+        case .JSON:
+            Alamofire.request(Method(rawValue: HTTPRequestType(rawValue: httpMethod.rawValue)!.name)!, requestURL, parameters: dataParameters, encoding: .JSON, headers: nil).responseJSON(completionHandler: { (JSONResponse) -> Void in
+                switch JSONResponse.result {
+                case .Success(let JSON):
+                    resultData(.Success(JSON))
+                case .Failure(let errorOccurred):
+                    resultData(.Failure(NetworkingError.ResponseError(errorOccurred.localizedDescription)))
                 }
             })
-        } else {
-            let requestURL = "\(setURL)\(endpoint!)"
-            Alamofire.request(Method(rawValue: HTTPRequestType(rawValue: httpMethod.rawValue)!.name)!, requestURL, parameters: dataParameters, encoding: .URL, headers: nil).responseJSON(completionHandler: { (jsonResponse) -> Void in
-                if let finalResponse = jsonResponse.result.value {
-                    JSONData(.Success(finalResponse))
-                } else {
-                    if let errorOccurred = jsonResponse.result.error {
-                        JSONData(.Failure(NetworkingError.ResponseError(errorOccurred.localizedDescription)))
-                    }
+        case .String:
+            Alamofire.request(Method(rawValue: HTTPRequestType(rawValue: httpMethod.rawValue)!.name)!, requestURL, parameters: dataParameters, encoding: .JSON, headers: nil).responseString(completionHandler: { (responseString) -> Void in
+                switch responseString.result {
+                case .Success(let validString):
+                    resultData(.Success(validString))
+
+                case .Failure(let errorOccurred):
+                    resultData(.Failure(NetworkingError.ResponseError(errorOccurred.localizedDescription)))
+                }
+            })
+        case .RawData:
+            Alamofire.request(Method(rawValue: HTTPRequestType(rawValue: httpMethod.rawValue)!.name)!, requestURL, parameters: dataParameters, encoding: .JSON, headers: nil).responseData({ (responseData) -> Void in
+                switch responseData.result {
+                case .Success(let validResponse):
+                    resultData(.Success(validResponse))
+                case .Failure(let errorOccurred):
+                    resultData(.Failure(NetworkingError.ResponseError(errorOccurred.localizedDescription)))
                 }
             })
         }
