f939e3a42fc2eb232016ebb20dc0c8c618dd08c8
Kemar White <gailsx@gmail.com>
2016-06-27 11:33:43 -0400

    Pod install and added travis setup file

--- Podfile
+++ Podfile
@@ -4,5 +4,9 @@ platform :ios, '8.0'
 # To enable Swift frameworks
 use_frameworks!
 
+target 'HTZNetworkingManager' do
+
 # Pods
 pod 'Alamofire'
+
+end
---
+++ Pods/Target Support Files/Pods-HTZNetworkingManager/Pods-HTZNetworkingManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HTZNetworkingManager : NSObject
+@end
+@implementation PodsDummy_Pods_HTZNetworkingManager
+@end
